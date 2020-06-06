50887f2ba8e6ceece218dad2436b1f6bffee9016
Lokesh <lokeshgaria8811@gmail.com>
2020-06-06 15:30:35 +0530

    drumkit by javascript
    Hello guys in this is Drum kit by javascript

---
+++ index.js
@@ -0,0 +1,36 @@
+ 
+
+function crash(){
+    var audio = new Audio('./sounds/crash.mp3');
+audio.play();
+}
+
+function kick(){
+    var audio = new Audio('./sounds/kick-bass.mp3');
+audio.play();
+}
+
+function snare(){
+    var audio = new Audio('./sounds/snare.mp3');
+audio.play();
+}
+
+function tom1(){
+    var audio = new Audio('./sounds/tom-1.mp3');
+audio.play();
+}
+
+function tom2(){
+    var audio = new Audio('./sounds/tom-2.mp3');
+audio.play();
+}
+
+function tom3(){
+    var audio = new Audio('./sounds/tom-3.mp3');
+audio.play();
+}
+
+function tom4(){
+    var audio = new Audio('./sounds/tom-4.mp3');
+audio.play();
+}
